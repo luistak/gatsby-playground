754b9858bf1133d81f0ea931ab0bfb641b341122
Luis <takahashihideki408@gmail.com>
2020-01-11 19:17:25 -0300

    bla

---
+++ hello-world/src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const App = () => {
+  return <h1>My appy</h1>
+}
+export default () => (
+  <div style={{ color: `purple`, fontSize: `72px` }}>
+    <App />
+  </div>
+)
